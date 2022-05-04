a0c166d61f8bf5ba1834ac200a134a167f03066c
Swarnendu Chatterjee <swarnendubabai32@gmail.com>
2022-05-05 01:45:50 +0530

    updated

--- s3.js
+++ s3.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 const fs = require('fs')
 const S3 = require('aws-sdk/clients/s3')
 
-const bucketName = process.env.AWS_BUCKET_NAME
-const region = process.env.AWS_BUCKET_REGION
-const accessKeyId = process.env.AWS_ACCESS_KEY
-const secretAccessKey = process.env.AWS_SECRET_KEY
+const bucketName = "sample-bucket-for-image-upload";
+const region = "us-west-2";
+const accessKeyId = "AKIA4WLWDJGGO3DBODPO";
+const secretAccessKey = "us/jDnTjhHn89H92qaKtBIOSavzInQLfyd0YeiXR";
 
 const s3 = new S3({
   region,
