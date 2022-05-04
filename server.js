05d4479b4becb8f48370fab5c59de822125ffaa4
Swarnendu Chatterjee <swarnendubabai32@gmail.com>
2022-05-05 01:50:57 +0530

    updated

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const { uploadFile, getFileStream } = require("./s3");
 
 const app = express();
 
-app.get("13.232.39.133:8080/images/:key", (req, res) => {
+app.get("/images/:key", (req, res) => {
     console.log(req.params);
     const key = req.params.key;
     const readStream = getFileStream(key);
@@ -19,22 +19,18 @@ app.get("13.232.39.133:8080/images/:key", (req, res) => {
     readStream.pipe(res);
 });
 
-app.post(
-    "13.232.39.133:8080/images",
-    upload.single("image"),
-    async (req, res) => {
-        const file = req.file;
-        console.log(file);
+app.post("/images", upload.single("image"), async (req, res) => {
+    const file = req.file;
+    console.log(file);
 
-        // apply filter
-        // resize
+    // apply filter
+    // resize
 
-        const result = await uploadFile(file);
-        await unlinkFile(file.path);
-        console.log(result);
-        res.send({ imagePath: `/images/${result.Key}` });
-        // res.send("Working")
-    }
-);
+    const result = await uploadFile(file);
+    await unlinkFile(file.path);
+    console.log(result);
+    res.send({ imagePath: `/images/${result.Key}` });
+    // res.send("Working")
+});
 
 app.listen(8080, () => console.log("listening on port 8080"));
